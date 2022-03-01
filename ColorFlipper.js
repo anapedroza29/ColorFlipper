1076d5e346816961b85231914457138477a16d66
anapedroza29 <100169738+anapedroza29@users.noreply.github.com>
2022-02-28 22:15:41 -0800

    Description of document.getElementById()

--- ColorFlipper.js
+++ ColorFlipper.js
@@ -1,33 +1,33 @@
+
 // arrays 
 let option = ["red","blue", "green","black","violet","brown"]
 console.log(option)
+
 // document.getElementById()
-// varible1 color that we are chenging 
-// varible2 to reference the html 
-// varrible3 for the id when the html id elemenent can be found 
-function changeTheColor (firstColor) {
-var elem = document.getElementById("id html");
-elem.style.option = firstColor;
+  // create a function "ChangeColor" 
+  // create a varible "elem" with the value of a specific ID 
+  // change the color of the variable "elem" to the color that was passed to the function
+function changeColor(firstColor) {
+  var elem = document.getElementById("hello-world");
+  elem.style.color = firstColor;
 }
+
 // document.querySelector()
-const color = document.querySelector(".color");
+let color = document.querySelector(".container div");
+console.log(color);
+
 // addEventListener()
-// function modifyText() {
-//    const blue = document.getElementById("blue");
-//    if (blue.firstChild.nodeValue == "three") {
-//      blue.firstChild.nodeValue = "two";
-//    } else {
-//      blue.firstChild.nodeValue = "three";
-//    }
-//  }
- // Add event listener to table
-const elem = document.getElementById("id html");
-//elem.addEventListener("click", function()); 
-console.log ("click", function())
-// //get random number between 0 - 3
-// const random = 2;
-// document.body.style.backgroundColor = color[randomNUmber];
-// color.textContent = color[ randomNUmber];
-// // Math.floor()
-// // Math.random()
- array.length
+
+
+// Add event listener to table
+
+
+// document.body.style.backgroundColor 
+
+//  Math.floor()
+
+
+//  Math.random()
+
+
+// array.length
