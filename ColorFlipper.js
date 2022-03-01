8ed08dade8beffb76ab22d7446da4c38f7af6c5e
anapedroza29 <100169738+anapedroza29@users.noreply.github.com>
2022-02-28 20:00:54 -0800

    initial files

---
+++ ColorFlipper.js
@@ -0,0 +1,33 @@
+// arrays 
+let option = ["red","blue", "green","black","violet","brown"]
+console.log(option)
+// document.getElementById()
+// varible1 color that we are chenging 
+// varible2 to reference the html 
+// varrible3 for the id when the html id elemenent can be found 
+function changeTheColor (firstColor) {
+var elem = document.getElementById("id html");
+elem.style.option = firstColor;
+}
+// document.querySelector()
+const color = document.querySelector(".color");
+// addEventListener()
+// function modifyText() {
+//    const blue = document.getElementById("blue");
+//    if (blue.firstChild.nodeValue == "three") {
+//      blue.firstChild.nodeValue = "two";
+//    } else {
+//      blue.firstChild.nodeValue = "three";
+//    }
+//  }
+ // Add event listener to table
+const elem = document.getElementById("id html");
+//elem.addEventListener("click", function()); 
+console.log ("click", function())
+// //get random number between 0 - 3
+// const random = 2;
+// document.body.style.backgroundColor = color[randomNUmber];
+// color.textContent = color[ randomNUmber];
+// // Math.floor()
+// // Math.random()
+// // array.length
