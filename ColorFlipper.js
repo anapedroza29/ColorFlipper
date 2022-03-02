5054bbaf9a1ca26b203e9e1a75b1e2e456067bc0
anapedroza29 <100169738+anapedroza29@users.noreply.github.com>
2022-03-01 22:44:29 -0800

    Adding now function to the project

--- ColorFlipper.js
+++ ColorFlipper.js
@@ -1,33 +1,40 @@
 
 // arrays 
-let option = ["red","blue", "green","black","violet","brown"]
-console.log(option)
+const colors = ["Red","Blue","Green","Violet","Brown"]
+//console.log(option)
 
 // document.getElementById()
   // create a function "ChangeColor" 
   // create a varible "elem" with the value of a specific ID 
   // change the color of the variable "elem" to the color that was passed to the function
 function changeColor(firstColor) {
-  var elem = document.getElementById("hello-world");
-  elem.style.color = firstColor;
-}
+var elem = document.getElementById("button");
+//elem.style.backgroundColor = firstColor
+const color = document.querySelector(".color");
+//document.body.style.backgroundColor = firstColor
+
+elem.addEventListener("click", () => {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+ })
+};
+
 
 // document.querySelector()
-let color = document.querySelector(".container div");
-console.log(color);
+
 
 // addEventListener()
 
 
-// Add event listener to table
-
-
-// document.body.style.backgroundColor 
+  //Math.random
+  
 
 //  Math.floor()
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
 
-
-//  Math.random()
 
 
 // array.length
