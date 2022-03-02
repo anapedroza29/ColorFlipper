1189e51bc30eb975a3d52e2498b516d8315d99ce
anapedroza29 <100169738+anapedroza29@users.noreply.github.com>
2022-03-02 15:25:12 -0800

    added new function

--- ColorFlipper.js
+++ ColorFlipper.js
@@ -1,40 +1,43 @@
 
-// arrays 
+// create an arrays of color
 const colors = ["Red","Blue","Green","Violet","Brown"]
-//console.log(option)
+ //console.log(colors)
 
 // document.getElementById()
   // create a function "ChangeColor" 
   // create a varible "elem" with the value of a specific ID 
   // change the color of the variable "elem" to the color that was passed to the function
 function changeColor(firstColor) {
-var elem = document.getElementById("button");
-//elem.style.backgroundColor = firstColor
+const elem = document.getElementById("button");
+elem.style.backgroundColor = ( firstColor)
+ //console.log (elem)
+
+
+// create a const color and asing document.querySelector()
+// Returns the first Element within the document that matches the specified selector ".color"
 const color = document.querySelector(".color");
-//document.body.style.backgroundColor = firstColor
+// console.log (color)
 
-elem.addEventListener("click", () => {
-  const randomNumber = getRandomNumber();
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
- })
-};
-
-
-// document.querySelector()
-
-
-// addEventListener()
-
-
-  //Math.random
-  
-
-//  Math.floor()
 function getRandomNumber() {
   return Math.floor(Math.random() * colors.length);
+
+}
+
+//call the var elem and execute a function when a user clicks on a button
+elem.addEventListener("click", getRandomNumber);
+const randomNumber = getRandomNumber();
+document.body.style.backgroundColor = colors[randomNumber];
+console.log (randomNumber)
+color.textContent = colors[randomNumber];
 }
 
 
 
-// array.length
+
+// function getRandomNumber() {
+//   return Math.floor(Math.random() * colors.length);
+// }
+// console.log(colors.length)
+
+
+
