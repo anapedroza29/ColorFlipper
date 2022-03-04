e104ecd74e1d2591ad1db9bc6fbb0f80507ea764
anapedroza29 <100169738+anapedroza29@users.noreply.github.com>
2022-03-03 17:19:20 -0800

    add connection between html and css

--- ColorFlipper.js
+++ ColorFlipper.js
@@ -7,37 +7,41 @@ const colors = ["Red","Blue","Green","Violet","Brown"]
   // create a function "ChangeColor" 
   // create a varible "elem" with the value of a specific ID 
   // change the color of the variable "elem" to the color that was passed to the function
-function changeColor(firstColor) {
+function changeColor(blue) {
 const elem = document.getElementById("button");
-elem.style.backgroundColor = ( firstColor)
+elem.style.backgroundColor = blue
  //console.log (elem)
 
 
 // create a const color and asing document.querySelector()
-// Returns the first Element within the document that matches the specified selector ".color"
+// asing first Element in the document that matches the specified selector ".color"
 const color = document.querySelector(".color");
-// console.log (color)
+
+
+//call the var elem and execute a function when a user clicks on a button
+elem.addEventListener("click", getRandomNumber);
+
+// create a function getRandomNumber
+// return Math floor random colors
 
 function getRandomNumber() {
   return Math.floor(Math.random() * colors.length);
 
 }
 
-//call the var elem and execute a function when a user clicks on a button
-elem.addEventListener("click", getRandomNumber);
+// create a const randomNumber and asing getRandomNumber
 const randomNumber = getRandomNumber();
+
+// Change the color of the background with a ramdom colors
 document.body.style.backgroundColor = colors[randomNumber];
-console.log (randomNumber)
+//console.log (randomNumber)
+
+// call const color and change the text of the first element with class="color"r with a ramdom colors
 color.textContent = colors[randomNumber];
 }
 
 
 
-
-// function getRandomNumber() {
-//   return Math.floor(Math.random() * colors.length);
-// }
-// console.log(colors.length)
 
 
 
